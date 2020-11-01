0100e7b46a7dbace8b6431491fc1d1a042a642dd
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2020-11-02 02:47:25 +0500

    sad

---
+++ src/init_firebase.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCM4A6BGqt87JdIUjyZWAoiBUnLT3I0yf8",
+    authDomain: "reactwithfirebase-9dab9.firebaseapp.com",
+    databaseURL: "https://reactwithfirebase-9dab9.firebaseio.com",
+    projectId: "reactwithfirebase-9dab9",
+    storageBucket: "reactwithfirebase-9dab9.appspot.com",
+    messagingSenderId: "980353424125",
+    appId: "1:980353424125:web:8a8663c9ecba2c93573c94",
+    measurementId: "G-XJ4SWS5XC8"
+  };
+
+firebase.initializeApp(firebaseConfig);
